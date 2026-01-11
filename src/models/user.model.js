6037700cd470e57d85adbc86995d140224eedce4
Pratham Khare <prathamkhare222@gmail.com>
2026-01-11 10:59:10 +0530

    add changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,7 +64,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next(); //only work when password is added or updated
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
